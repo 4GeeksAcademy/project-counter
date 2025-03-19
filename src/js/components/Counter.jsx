d10c480078fc11280b4411b296064fe636374375
Moyunlimited <moy.estime@gmail.com>
2025-03-19 19:50:31 +0000

    done

--- src/js/components/Counter.jsx
+++ src/js/components/Counter.jsx
@@ -4,7 +4,7 @@ import React from "react";
 //create your first component
 const Counter = (props) => {
 	return (
-		<div className="text-center d-flex justify-content-center">
+		<div className="text-center d-flex justify-content-center mt-5">
             <div className="hours">{Math.floor(props.seconds/3600)%60}</div>
             <div className="minutes px-2">{Math.floor(props.seconds/60)%60}</div>
             <div className="seconds px-2">{Math.floor(props.seconds%60)}</div>
